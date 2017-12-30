d8c4cf424a82d23b22ebe9e04c7519cca728aaf4
keva91 <keva.91@hotmail.fr>
2017-12-30 22:05:33 +0100

    before merge

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,20 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Assertions;
 using UnityEngine;
 using SocketIO;
 
 public class Player : MonoBehaviour {
 
-    public SocketIOComponent socket;
 
-    
-      
+    private SocketIOComponent socket;
 
-	// Use this for initialization
-	void Start () {
-        socket.Emit("Connexion");
-		
-	}
-	
-	
+    // Use this for initialization
+    void Start () {
+
+        GameObject go = GameObject.Find("SocketIO");
+        socket = go.GetComponent<SocketIOComponent>();
+
+       
+
+        socket.Emit("connexion");
+
+        socket.On("message", (SocketIOEvent e) => {
+            print("message ");
+            Debug.Log("message");
+            Debug.Log(e);
+        });
+
+
+    }
+
+
+   
+
+
+    void Update()
+    {
+        //socket.On("message", (SocketIOEvent e) => {
+        //    print("message ");
+        //    Debug.Log("message");
+        //    Debug.Log(e);
+        //});
+    }
+
+    void connectM(SocketIOEvent iOEvent)
+    {
+        print("connected ");
+        print(iOEvent);
+    }
+
 }
